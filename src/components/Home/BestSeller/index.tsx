9c5bc99b0d779f239c196eb015c893fff72ecfd7
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-08-15 13:27:00 +0530

    bug this month text is removed

--- src/components/Home/BestSeller/index.tsx
+++ src/components/Home/BestSeller/index.tsx
@@ -27,7 +27,7 @@ const BestSeller = () => {
         {/* <!-- section title --> */}
         <div className="mb-10 flex items-center justify-between">
           <div>
-            <span className="flex items-center gap-2.5 font-medium text-dark mb-1.5">
+            {/*<span className="flex items-center gap-2.5 font-medium text-dark mb-1.5">
               <Image
                 src="/images/icons/icon-07.svg"
                 alt="icon"
@@ -35,7 +35,7 @@ const BestSeller = () => {
                 height={17}
               />
               This Month
-            </span>
+            </span> */}
             <h2 className="font-semibold text-xl xl:text-heading-5 text-dark">
               Best Sellers
             </h2>
