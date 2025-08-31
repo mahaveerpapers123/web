3381b5377bc2b6d970b4d0e7d62a837397bd4f0b
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-08-31 23:34:57 +0530

    navlinks dropdown completed

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,8 +2,52 @@
 import Link from "next/link";
 import React, { useState } from "react";
 
+const ArrowDown = ({ open }) => (
+  <svg
+    className={`fill-current ease-in-out duration-200 ${open ? "rotate-180" : ""}`}
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M6.0001 7.8248L10.2426 3.5823L11.1854 4.5251L6.0001 9.7104L0.814786 4.5251L1.75759 3.5823L6.0001 7.8248Z" />
+  </svg>
+);
+
+const ArrowRight = () => (
+  <svg
+    className="fill-current"
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M3.5823 1.75759L7.8248 6.0001L3.5823 10.2426L2.63949 9.3L6.93949 6.0001L2.63949 2.7001L3.5823 1.75759Z" />
+  </svg>
+);
+
 const Dropdown = ({ menuItem, stickyMenu }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const renderNode = (node) => {
+    const hasChildren = Array.isArray(node.submenu) && node.submenu.length > 0;
+    return (
+      <li key={node.slugKey || node.title} className="relative group">
+        <Link
+          href={`/shopping?category=${node.path}`}
+          className="flex items-center justify-between rounded px-3.5 py-2 text-sm font-medium text-dark hover:bg-gray-100 hover:text-blue"
+        >
+          {node.title}
+          {hasChildren && <ArrowRight />}
+        </Link>
+        {hasChildren && (
+          <ul className="absolute left-full top-0 z-20 min-w-[220px] rounded-md bg-white p-3 shadow-lg opacity-0 invisible transition-all duration-300 group-hover:opacity-100 group-hover:visible">
+            {node.submenu.map((child) => renderNode(child))}
+          </ul>
+        )}
+      </li>
+    );
+  };
 
   return (
     <li
@@ -17,26 +61,11 @@ const Dropdown = ({ menuItem, stickyMenu }) => {
           stickyMenu ? "xl:py-4" : "xl:py-6"
         }`}
         onClick={(e) => {
-          if (menuItem.submenu) {
-            e.preventDefault();
-          }
+          if (menuItem.submenu) e.preventDefault();
         }}
       >
         {menuItem.title}
-        {menuItem.submenu && (
-          <svg
-            className={`fill-current ease-in-out duration-200 ${
-              dropdownOpen && "rotate-180"
-            }`}
-            width="12"
-            height="12"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M6.0001 7.8248L10.2426 3.5823L11.1854 4.5251L6.0001 9.7104L0.814786 4.5251L1.75759 3.5823L6.0001 7.8248Z" />
-          </svg>
-        )}
+        {menuItem.submenu && <ArrowDown open={dropdownOpen} />}
       </a>
 
       {menuItem.submenu && (
@@ -45,42 +74,7 @@ const Dropdown = ({ menuItem, stickyMenu }) => {
             dropdownOpen ? "opacity-100 visible" : "opacity-0 invisible"
           }`}
         >
-          {menuItem.submenu.map((subItem, index) => (
-            <li key={index} className="group/submenu relative">
-              <Link
-                href={`/shopping?category=${subItem.path}`}
-                className="flex items-center justify-between rounded px-3.5 py-2 text-sm font-medium text-dark hover:bg-gray-100 hover:text-blue"
-              >
-                {subItem.title}
-                {subItem.submenu && (
-                  <svg
-                    className="fill-current"
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M3.5823 1.75759L7.8248 6.0001L3.5823 10.2426L2.63949 9.3L6.93949 6.0001L2.63949 2.7001L3.5823 1.75759Z" />
-                  </svg>
-                )}
-              </Link>
-
-              {subItem.submenu && (
-                <ul className="absolute left-full top-0 z-20 min-w-[220px] rounded-md bg-white p-3 shadow-lg opacity-0 invisible transition-all duration-300 group-hover/submenu:opacity-100 group-hover/submenu:visible">
-                  {subItem.submenu.map((nestedItem, nestedIndex) => (
-                    <li key={nestedIndex}>
-                      <Link
-                        href={`/shopping?category=${nestedItem.path}`}
-                        className="block rounded px-3.5 py-2 text-sm font-medium text-dark hover:bg-gray-100 hover:text-blue"
-                      >
-                        {nestedItem.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
+          {menuItem.submenu.map((sub) => renderNode(sub))}
         </ul>
       )}
     </li>
